08053616e706491eada0779662c757c9b0ca0bc1
vinoosha <60358570+vinoosha@users.noreply.github.com>
2020-07-22 10:26:22 +0530

    Add files via upload

---
+++ src/main/java/streams/avgnumbers.java
@@ -0,0 +1,15 @@
+package streams;
+import java.util.Arrays;
+import java.util.List;
+
+public class avgnumbers 
+{
+	public static void main(String args[]) {
+		List<Integer> dou=Arrays.asList(1,2,3,4,5);
+		double avg=dou.stream().
+		mapToInt(i->i).
+		average().orElse(-1);
+		System.out.println("Average of numbers is: "+avg);
+		
+	}
+}
